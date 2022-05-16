572f600636af93526a2db4c179cd691bac79a627
Haaris Chaudhry <haaris.h.chaudhry@gmail.com>
2022-05-16 17:37:02 -0500

    Move movie description to different file, add styling to new file

---
+++ lib/screens/movie_description.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../models/movie.dart';
+
+class MovieDescription extends StatelessWidget {
+  const MovieDescription({
+    Key? key,
+    required this.movie,
+  }) : super(key: key);
+
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: const BoxDecoration(color: Colors.black54),
+      child: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          children: [
+            Container(
+              child: Text(
+                movie.title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                ),
+              ),
+              margin: EdgeInsets.only(bottom: 20),
+            ),
+            Text(
+              movie.overview,
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ],
+          crossAxisAlignment: CrossAxisAlignment.start,
+        ),
+      ),
+    );
+  }
+}
